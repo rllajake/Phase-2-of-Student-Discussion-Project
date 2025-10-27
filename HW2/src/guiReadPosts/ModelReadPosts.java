dad7d0c2ed752c74d7cb274468eae75a8f89726f
Jake Thomasian <158413797+rllajake@users.noreply.github.com>
2025-10-27 15:27:28 -0700

    Updated MVC Classes for guiReadPosts
    Added Javadoc.
    
    Signed-off-by: Jake Thomasian <158413797+rllajake@users.noreply.github.com>

--- HW2/src/guiReadPosts/ModelReadPosts.java
+++ HW2/src/guiReadPosts/ModelReadPosts.java
@@ -1,5 +1,18 @@
 package guiReadPosts;
 
+
+
+/*******
+ * <p> Title: ModelReadPosts Class. </p>
+ * 
+ * <p> Description: Model for the Read Posts feature. Holds transient UI state relevant to the listing and selection of posts.</p>
+ * 
+ * <p> Copyright: Group 14 © 2025 </p>
+ * 
+ * @author Group 14
+ * 
+ * @version 1.00		2025-10-24 Initial documentation alignment for guiReadPosts
+ */
 public class ModelReadPosts {
 
 }
