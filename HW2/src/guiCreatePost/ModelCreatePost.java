3801644cad4be1828625d3ea02ffbd53e5382c49
Maverick Madison <134321883+Gawdlie@users.noreply.github.com>
2025-10-27 16:06:25 -0700

    Enhance ModelCreatePost with documentation comments
    Added documentation comments to the ModelCreatePost class and its constructor.
    
    Signed-off-by: Maverick Madison <134321883+Gawdlie@users.noreply.github.com>

--- HW2/src/guiCreatePost/ModelCreatePost.java
+++ HW2/src/guiCreatePost/ModelCreatePost.java
@@ -1,5 +1,22 @@
 package guiCreatePost;
 
-public class ModelCreatePost {
 
+/*******
+ * <p> Title: ModelCreatePost Class. </p>
+ * 
+ * <p> Description: The CreatePost Page Model.  This class is not used as there is no
+ * data manipulated by this MVC beyond accepting role information and saving it in the
+ * database.</p>
+ *  
+ */
+public class ModelCreatePost {
+	/*****
+	 * <p> Title: ModelCreatePost() Constructor </p>
+	 * 
+	 * <p> Description: Default Constructor for Model Create Posts, not used here in this application. </p>
+	 * 
+	 */
+	public ModelCreatePost() {
+		
+	}
 }
